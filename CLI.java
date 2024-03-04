8681441fb2fcc27df652e0f200010f68310e7236
Tanmay Patil <tnp1210@g.rit.edu>
2024-03-04 21:44:32 +0400

    Seventh Commit

---
+++ CLI.java
@@ -0,0 +1,33 @@
+package peggame;
+
+public class CLI implements PegGame{
+
+    private PegGame gameInstance;
+
+    public CLI(String filename){
+        
+    }
+        
+    
+    @Override
+    public GameState getGameState() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection <Move> getPossibleMoves() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void makeMove(Move move) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    public static void playPeg(){
+
+    }
+}
