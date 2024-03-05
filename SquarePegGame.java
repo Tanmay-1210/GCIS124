afa8557766e97b27d6e972b0cb62be5179f999f2
Tanmay Patil <tnp1210@g.rit.edu>
2024-03-05 22:55:10 +0400

    Final Commit for the File

--- SquarePegGame.java
+++ SquarePegGame.java
@@ -1,62 +1,25 @@
-package peggame;
-
 import java.util.ArrayList;
 import java.util.Collection;
-
+    //Using SquarePegGame class with PegGame interface
 public class SquarePegGame implements PegGame {
-
-    private BoardShape board;
+    //Initializing variable with datatypes
+    private Location[][] board;
     private GameState current_state;
 
-    public SquarePegGame(){
-        this.current_state=GameState.NOT_STARTED;
-        this.board=new BoardShape();
+    //Initializing constructor for the class
+    public SquarePegGame(int row, int col ) {
+        this.current_state = GameState.NOT_STARTED;
+        this.board = new Location[row][col];
+    }
+    
+    //initializing getters
+    public Location[][] getBoard() {
+        return board;
     }
 
+    //Initializing getters
     @Override
     public GameState getGameState() {
         return current_state;
     }
-
-    @Override
-    public Collection<Move> getPossibleMoves() {
-        Collection<Move> collection = new ArrayList<>();
-        Location[][] temporaryBoard= board.getBoard();
-        for (int row= 0; row <temporaryBoard.length; row++){
-            for (int column=0; column< temporaryBoard[row].length; column++){
-                if (temporaryBoard[row][column]!=null){
-                    for(){
-                        
-                    }
-                    
-                    if(row-2>=0&& column-2>=0){
-
-                    }
-                    if ()
-
-                }
-            }
-        }
-    
-        return collection;
-    }
-
-    @Override
-    public void makeMove(Move move) throws PegGameException {// not sure how to do that either
-        
-            if (getPossibleMoves().contains(move)){
-            
-                return;
-            } else {
-                throw new PegGameException("invalid move");
-            }
-                
-        
-    }
-
-    @Override
-    public String toString() {
-        return ""; //check if my BoardShape class is good, if  it's then make the toString method 
-    }
-
 }
